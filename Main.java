b708e683b6719ddeeecc68dad4cd30f413de6738
Зоригто Жеребцов <zorigto82@mail.ru>
2022-05-12 06:45:58 +0300

    Iterator added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int r : new Randoms(90, 100)) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
+}
